8c057965a9962e5c648a0a2de18303e06dfcb660
patryksladek <patryksladek@wp.pl>
2021-12-11 00:30:21 +0100

    Preparing the project

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using OrderProcessor.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using OrderProcessor.Dtos;
+
+namespace OrderProcessor.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ICustomersRepository _customersRepository;
+        private readonly IOrdersRepository _ordersRepository;
+        public CustomersController(ICustomersRepository customersRepository, IOrdersRepository ordersRepository)
+        {
+            _customersRepository = customersRepository;
+            _ordersRepository = ordersRepository;
+        }
+        
+        [HttpGet("")]
+        public async Task<IActionResult> GetCustomers()
+        {
+            var customersDto = await _customersRepository.GetCustomersAsync();
+
+            return Ok(customersDto);
+        }
+
+        [HttpGet("{customerId}")]
+        public async Task<IActionResult> GetCustomer(Guid customerId)
+        {
+            var customerDto = await _customersRepository.GetCustomerAsync(customerId);
+
+            if (customerDto == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customerDto);
+        }
+
+        [HttpGet("{customerId}/orders")]
+        public async Task<IActionResult> GetCustomerOrders(Guid customerId)
+        {
+            var ordersDto = await _ordersRepository.GetOrdersAsync();
+            var custmerOrders = new CustomerOrdersDto
+            {
+                CustomerId = customerId,
+                Orders = ordersDto.Where(x => x.Customer.Id == customerId).ToList()
+            };
+
+            return Ok(custmerOrders);
+        }
+    }
+}
---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using OrderProcessor.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OrderProcessor.Dtos;
+
+namespace OrderProcessor.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrdersRepository _ordersRepository;
+        private readonly ILogger<OrdersController> _logger;
+
+        public OrdersController(IOrdersRepository ordersRepository, ILogger<OrdersController> logger)
+        {
+            _ordersRepository = ordersRepository;
+            _logger = logger;
+        }
+        
+        [HttpPost("")]
+        public async Task<IActionResult> CreateOrder([FromBody] CreateCustomerOrderDto createCustomerOrder)
+        {
+            var order = await _ordersRepository.CreateOrderAsync(createCustomerOrder.CustomerId, createCustomerOrder.ProductId);
+            _logger.LogInformation($"Created order for customer: {order.Customer.Id} for product: {order.Product.Id}");
+
+            return CreatedAtAction("GetOrder", new { orderId = order.Id }, order);
+        }
+
+        [HttpGet("{orderId}")]
+        public async Task<IActionResult> GetOrder(Guid orderId)
+        {
+            var orderDto = await _ordersRepository.GetOrderAsync(orderId);
+
+            if (orderDto == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(orderDto);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllOrders()
+        {
+            var ordersDto = await _ordersRepository.GetOrdersAsync();
+            return Ok(ordersDto);
+        }
+    }
+}
